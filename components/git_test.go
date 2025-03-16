c80172f260bc088102a371f221249db3f93eb033
Mccullahz <zylardmccullah@gmail.com>
2025-03-16 15:13:31 -0400

    adjusting testing a little, adding initial git test

---
+++ components/git_test.go
@@ -0,0 +1,16 @@
+package components
+
+import (
+	//"strings"
+	"testing"
+)
+
+// both test functions are just placeholders for now, once git.go is more setup will introduce real test cases
+func TestGitPull(t *testing.T) {
+
+}
+
+
+func TestGitPush(t *testing.T) {
+
+}
